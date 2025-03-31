2c504ea09243b9d045755ebc779405200978df6e
Cherry Wang <cherry@iotechsys.com>
2025-03-31 17:27:02 +0800

    show docker logs
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/utils/scripts/docker/deploy-edgex.sh
+++ TAF/utils/scripts/docker/deploy-edgex.sh
@@ -10,32 +10,14 @@ if [ "$TEST_STRATEGY" = "PerformanceMetrics" ]; then
           --env-file ${WORK_DIR}/TAF/utils/scripts/docker/common-taf.env ${COMPOSE_IMAGE} docker compose \
           -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose${APPSERVICE}.yml" up -d
 else
-  for PROFILE in device-virtual device-modbus device-modbus_1; do
-    docker run --rm -v ${WORK_DIR}:${WORK_DIR}:rw,z -w ${WORK_DIR} -v /var/run/docker.sock:/var/run/docker.sock \
-           --security-opt label:disable --env-file ${WORK_DIR}/TAF/utils/scripts/docker/common-taf.env \
-           --env CONFIG_DIR=${CONFIG_DIR} --env WORK_DIR=${WORK_DIR} ${COMPOSE_IMAGE} \
-           docker compose -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose.yml" up --no-start --no-deps ${PROFILE}
-
-    # copy device service default configuration in the res to TAF/config/{service}/res
-    docker cp edgex-${PROFILE}:/res/configuration.yaml ${WORK_DIR}/TAF/config/${PROFILE}
-    sed -i "s/Device:/Device_old:/" ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
-    echo "" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
-    echo "Device:" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
-    echo "  ProfilesDir: \"$CONFIG_DIR\"" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
-    echo "  DevicesDir: \"$CONFIG_DIR\"" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
-
-    ## To set messagebus clientId
-    if [ "$PROFILE" = "device-modbus_1" ]; then
-      sed -i "s/device-modbus/device-modbus_1/" ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
-    fi
-
-  done
-
   docker run --rm -v ${WORK_DIR}:${WORK_DIR}:rw,z -w ${WORK_DIR} -v /var/run/docker.sock:/var/run/docker.sock \
           --env-file ${WORK_DIR}/TAF/utils/scripts/docker/common-taf.env \
           --add-host=host.docker.internal:host-gateway \
           --env WORK_DIR=${WORK_DIR} --env CONFIG_DIR=${CONFIG_DIR} --security-opt label:disable ${COMPOSE_IMAGE} \
           docker compose -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose.yml" up -d
+  echo "================================="
+  docker logs edgex-core-keeper
+  docker logs edgex-core-data
 fi
 
 # Waiting for all services startup
