86b6ff28cb8db56282f696e0a9ccd9a70206ac5b
Cherry Wang <cherry@iotechsys.com>
2025-11-24 17:02:19 +0800

    test odessa-dev
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/utils/scripts/docker/sync-compose-file.sh
+++ TAF/utils/scripts/docker/sync-compose-file.sh
@@ -11,7 +11,8 @@ TAF_PERF=$4
 # Handle TAF specific compose files
 [ "$TAF" = "-taf" ] && TAF_SUB_FOLDER="/taf"
 
-NIGHTLY_BUILD_URL="https://raw.githubusercontent.com/edgexfoundry/edgex-compose/${USE_SHA1}${TAF_SUB_FOLDER}"
+#NIGHTLY_BUILD_URL="https://raw.githubusercontent.com/edgexfoundry/edgex-compose/${USE_SHA1}${TAF_SUB_FOLDER}"
+NIGHTLY_BUILD_URL="https://raw.githubusercontent.com/cherrycl/edgex-compose/${USE_SHA1}${TAF_SUB_FOLDER}"
 
 COMPOSE_FILE="docker-compose${TAF}${TAF_PERF}${USE_NO_SECURITY}.yml"
 curl -o ${COMPOSE_FILE} "${NIGHTLY_BUILD_URL}/${COMPOSE_FILE}"
